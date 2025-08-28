7ee50ca02fb9731921f2993e8beefb281b1a35a7
satish.avunoori <satish.avunoori@AHLPT2305001-R.ADVINTEK.LOCAL>
2025-08-28 16:46:07 +0530

    changed

--- einovice-bdd/src/main/java/com/einovice/App.java
+++ einovice-bdd/src/main/java/com/einovice/App.java
@@ -4,20 +4,13 @@ import io.cucumber.core.cli.Main;
 
 public class App {
     public static void main(String[] args) {
-        // Arguments to run Cucumber CLI
-    	String[] cucumberOptions = new String[] {
-    		    "--glue", "com.einovice.stepdefinitions",
-    		    "src/test/resources/features"
-    		};
-        
-        
-        
-        
+        // Corrected arguments to run Cucumber CLI
+        String[] cucumberOptions = new String[] {
+            "--glue", "com.einovice.stepdefinitions",
+            "classpath:features"
+        };
 
-        // Run Cucumber
         byte exitStatus = Main.run(cucumberOptions, Thread.currentThread().getContextClassLoader());
-
-        // Exit with the appropriate status
         System.exit(exitStatus);
     }
 }
