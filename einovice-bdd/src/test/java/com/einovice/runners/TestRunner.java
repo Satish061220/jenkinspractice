0cebbeeb5d6c19e3c1e69f7fceaf952d917efeb8
satish.avunoori <satish.avunoori@AHLPT2305001-R.ADVINTEK.LOCAL>
2025-08-29 10:06:20 +0530

    changed test runner file

--- einovice-bdd/src/test/java/com/einovice/runners/TestRunner.java
+++ einovice-bdd/src/test/java/com/einovice/runners/TestRunner.java
@@ -1,6 +1,28 @@
 package com.einovice.runners;
 
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features", // consistent with Maven and Jenkins
+    glue = "com.einovice.stepdefinitions",
+    plugin = {
+        "pretty",
+        "html:target/cucumber-report.html",          // basic HTML report
+        "json:target/cucumber.json",                 // needed for advanced reports
+        "junit:target/cucumber.xml"                  // optional for Jenkins integration
+    },
+    monochrome = true                                // cleaner console output
+)
+public class TestRunner {
+}
+
+
+
 /*
  * import io.cucumber.junit.Cucumber; import io.cucumber.junit.CucumberOptions;
  * import org.junit.runner.RunWith;
@@ -17,18 +39,16 @@ package com.einovice.runners;
  * 
  * 
  * public class TestRunner { }
+ * 
+ * 
+ * 
+ * 
+ * import io.cucumber.junit.Cucumber; import io.cucumber.junit.CucumberOptions;
+ * import org.junit.runner.RunWith;
+ * 
+ * @RunWith(Cucumber.class)
+ * 
+ * @CucumberOptions( features = "classpath:features", glue =
+ * "com.einovice.stepdefinitions", plugin = {"pretty",
+ * "html:target/cucumber-report.html"} ) public class TestRunner { }
  */
-
-
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-  features = "classpath:features",
-  glue = "com.einovice.stepdefinitions",
-  plugin = {"pretty", "html:target/cucumber-report.html"}
-)
-public class TestRunner { }
