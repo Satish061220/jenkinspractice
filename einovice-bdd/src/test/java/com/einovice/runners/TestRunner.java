60192ca7ce810e6bf5038b95ef50222acb481b1a
satish.avunoori <satish.avunoori@host.docker.internal>
2025-09-03 13:39:23 +0530

    updated runner file

--- einovice-bdd/src/test/java/com/einovice/runners/TestRunner.java
+++ einovice-bdd/src/test/java/com/einovice/runners/TestRunner.java
@@ -6,7 +6,21 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
+/*@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/main/resources/features", // consistent with Maven and Jenkins
+    glue = "com.einovice.stepdefinitions",
+    plugin = {
+        "pretty",
+        "html:target/cucumber-report.html",          // basic HTML report
+        "json:target/cucumber.json",                 // needed for advanced reports
+        "junit:target/cucumber.xml"                  // optional for Jenkins integration
+    },
+    monochrome = true                                // cleaner console output
+)
+public class TestRunner {
+}
+*/
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
