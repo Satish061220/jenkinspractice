3f10ea06fa30c0f8e8d797a13eb5585d6d59bf5d
satish.avunoori <satish.avunoori@host.docker.internal>
2025-10-13 12:30:39 +0530

    hellloo checking

--- einovice-bdd/src/main/java/com/einovice/App.java
+++ einovice-bdd/src/main/java/com/einovice/App.java
@@ -5,7 +5,7 @@ package com.einovice;
 import io.cucumber.core.cli.Main;
 
 public class App {
-    public static void main(String[] args) {//comment 
+    public static void main(String[] args) {//comment hellooooo
         // Point to the classpath for main/resources/features
         String[] cucumberOptions = new String[] {
             "--glue", "com.einovice.stepdefinitions",
